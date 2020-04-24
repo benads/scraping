31f676ffd93eba09b3b7c917cfde3a778ac6a6c3
Benjamin <benjaminad5@yahoo.com>
2020-04-24 13:21:34 +0200

    feat(API-GET)

---
+++ api.get.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+# GET request to an API
+response = requests.get("http://api.open-notify.org/iss-now.json")
+status_code = response.status_code
+data = response.content
+print(data)
+print(status_code)
+
+
