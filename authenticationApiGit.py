14bf411d56c0b9a1aed1f725f710aa3ceb6de370
Benjamin <benjaminad5@yahoo.com>
2020-04-25 22:26:25 +0200

    feat(API-GITHUB) another example

--- authenticationApiGit.py
+++ authenticationApiGit.py
@@ -1,5 +1,10 @@
 import requests
 
-headers = {"Authorization": 'token 6ec1888ef00d4a83b4f507b14adb017f5050cd44'}
+headers = {"Authorization": 'token 47137a2d0eb9a8cefcd1ed4c98858a50a122e9f5'}
 response = requests.get('http://api.github.com/users/benads', headers=headers)
 print(response.json())
+
+response = requests.get(
+    'http://api.github.com/repos/octocat/Hello-World', headers=headers)
+hello_world = response.json()
+print(hello_world)
